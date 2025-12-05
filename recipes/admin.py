209a237aa730da8de8b3683b7204f4040ba03d43
j-mlo <joanna.r.davis@outlook.com>
2025-12-05 06:23:51 +0000

    Register models in admin and add instnces on admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, Tag
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    list_filter = ("tags",)
+    search_fields = ("name",)
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Tag)
